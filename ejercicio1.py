97a5c7320a4dfd8c863f1b75f0c884c09ed88adb
Dragonimus <151642604+Dragonimus@users.noreply.github.com>
2024-04-24 09:22:35 -0300

    Create ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,9 @@
+numero_entero=10 
+numero_flotante=8,90
+cadena="hola word"
+es_verdadero= True
+print("numero entero:",numero_entero)
+print("numero flotante:",numero_flotante)
+print("cadena",cadena)
+print("¿es verdadero?",es_verdadero)
+
