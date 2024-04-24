8e3f15ad415f1850514f321d4eeaa6b363e0b634
Dragonimus <151642604+Dragonimus@users.noreply.github.com>
2024-04-24 09:24:05 -0300

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,12 @@
+O=12
+t=9
+suma=O + t
+resta=O + t
+multiplicacion=O * t
+division=O / t
+modulo=O % t
+comparacion_suma_multiplicacion = suma > multiplicacion
+comparacion_division_multiplicacion=multiplicacion < division
+print("resultado de la suma",suma)
+print("el resdultado de la suma es mayor que el de la multiplicacion ?", comparacion_suma_multiplicacion)
+print("el resdultado de la multiplicacion es menor que el de la division ?", comparacion_division_multiplicacion)
